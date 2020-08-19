c13025256ab6a8f9a97ff10c4ba9c58be26fb1e0
Dullly <Dullly@users.noreply.github.com>
2020-08-19 17:19:23 +0800

    age

---
+++ age.js
@@ -0,0 +1,214 @@
+[
+  {
+    "label": "18岁",
+    "value": 0
+  },
+  {
+    "label": "19岁",
+    "value": 1
+  },
+  {
+    "label": "20岁",
+    "value": 2
+  },
+  {
+    "label": "21岁",
+    "value": 3
+  },
+  {
+    "label": "22岁",
+    "value": 4
+  },
+  {
+    "label": "23岁",
+    "value": 5
+  },
+  {
+    "label": "24岁",
+    "value": 6
+  },
+  {
+    "label": "25岁",
+    "value": 7
+  },
+  {
+    "label": "26岁",
+    "value": 8
+  },
+  {
+    "label": "27岁",
+    "value": 9
+  },
+  {
+    "label": "28岁",
+    "value": 10
+  },
+  {
+    "label": "29岁",
+    "value": 11
+  },
+  {
+    "label": "30岁",
+    "value": 12
+  },
+  {
+    "label": "31岁",
+    "value": 13
+  },
+  {
+    "label": "32岁",
+    "value": 14
+  },
+  {
+    "label": "33岁",
+    "value": 15
+  },
+  {
+    "label": "34岁",
+    "value": 16
+  },
+  {
+    "label": "35岁",
+    "value": 17
+  },
+  {
+    "label": "36岁",
+    "value": 18
+  },
+  {
+    "label": "37岁",
+    "value": 19
+  },
+  {
+    "label": "38岁",
+    "value": 20
+  },
+  {
+    "label": "39岁",
+    "value": 21
+  },
+  {
+    "label": "40岁",
+    "value": 22
+  },
+  {
+    "label": "41岁",
+    "value": 23
+  },
+  {
+    "label": "42岁",
+    "value": 24
+  },
+  {
+    "label": "43岁",
+    "value": 25
+  },
+  {
+    "label": "44岁",
+    "value": 26
+  },
+  {
+    "label": "45岁",
+    "value": 27
+  },
+  {
+    "label": "46岁",
+    "value": 28
+  },
+  {
+    "label": "47岁",
+    "value": 29
+  },
+  {
+    "label": "48岁",
+    "value": 30
+  },
+  {
+    "label": "49岁",
+    "value": 31
+  },
+  {
+    "label": "50岁",
+    "value": 32
+  },
+  {
+    "label": "51岁",
+    "value": 33
+  },
+  {
+    "label": "52岁",
+    "value": 34
+  },
+  {
+    "label": "53岁",
+    "value": 35
+  },
+  {
+    "label": "54岁",
+    "value": 36
+  },
+  {
+    "label": "55岁",
+    "value": 37
+  },
+  {
+    "label": "56岁",
+    "value": 38
+  },
+  {
+    "label": "57岁",
+    "value": 39
+  },
+  {
+    "label": "58岁",
+    "value": 40
+  },
+  {
+    "label": "59岁",
+    "value": 41
+  },
+  {
+    "label": "60岁",
+    "value": 42
+  },
+  {
+    "label": "61岁",
+    "value": 43
+  },
+  {
+    "label": "62岁",
+    "value": 44
+  },
+  {
+    "label": "63岁",
+    "value": 45
+  },
+  {
+    "label": "64岁",
+    "value": 46
+  },
+  {
+    "label": "65岁",
+    "value": 47
+  },
+  {
+    "label": "66岁",
+    "value": 48
+  },
+  {
+    "label": "67岁",
+    "value": 49
+  },
+  {
+    "label": "68岁",
+    "value": 50
+  },
+  {
+    "label": "69岁",
+    "value": 51
+  },
+  {
+    "label": "70岁",
+    "value": 52
+  }
+]
